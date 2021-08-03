be0cbd369b1cc875560a3807c0db8122bac04c92
Kayvon Kazemini <kayvonk@gmail.com>
2021-08-03 02:29:52 -0600

    Updated tailwinds purge path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // purge: [],
   purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
