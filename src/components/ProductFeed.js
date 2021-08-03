7442d71c858b31ecd6f7120a3fc0c0c5cd16ccd6
Kayvon Kazemini <kayvonk@gmail.com>
2021-08-03 00:43:20 -0600

    First deploy

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Product from "./Product";
+
+export default function ProductFeed({ products }) {
+  return (
+    <div className="grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 md:-mt-10 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <img
+        className="col-span-full w-screen"
+        src="images/adventure.png"
+        alt="travel-ad"
+      />
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+    </div>
+  );
+}
